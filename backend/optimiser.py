0552cf807457d006a2355442d0c28e93316a52ee
IIInitiationnn <namey6@gmail.com>
2023-05-13 18:24:01 +1000

    fixed optimiser to only calculate relevant paths (1 accessible node) to relevant destination nodes (accessible)

--- backend/optimiser.py
+++ backend/optimiser.py
@@ -18,6 +18,7 @@ class Optimiser:
         self.shortest_paths = dict(nx.all_pairs_shortest_path(self.base_graph))
         self.dijkstra_graph = None
 
+    # deprecated
     def dijkstra(self, node_id, tier, subtier):
         # run dijkstra map for node corresponding to node_id
         heatmap = copy.deepcopy(self.base_graph)
@@ -57,7 +58,7 @@ class Optimiser:
     def dijkstra_multiplier(self, src_node, tier, subtier):
         heatmap = copy.deepcopy(self.base_graph)
 
-        # the lower this number, the higher the priority
+        # the lower this number, the higher the priority (will always be negative!)
         desired_value = -(Optimiser.TIER_VALUE * tier + subtier * Optimiser.SUBTIER_VALUE)
         base_data = self.base_graph.nodes[src_node]
         nx.set_node_attributes(heatmap, GraphNode.from_dict(base_data, value=desired_value).get_dict())
@@ -66,10 +67,19 @@ class Optimiser:
         if heatmap.nodes[src_node]["cls_name"] == NodeType.ACCESSIBLE:
             return heatmap
 
-        for dst_node in self.shortest_paths[src_node].keys():
-            if dst_node == src_node:
+        for dst_node in self.shortest_paths[src_node].keys(): # for every path from source node
+            if dst_node == src_node: # ignore trivial paths
                 continue
+
+            # path must be to relevant dst (dst must be accessible, with no other accessible nodes along the way)
+            if heatmap.nodes[dst_node]["cls_name"] != NodeType.ACCESSIBLE:
+                continue
+            if len([1 for intermediate_node in self.shortest_paths[src_node][dst_node]
+                   if heatmap.nodes[intermediate_node]["cls_name"] == NodeType.ACCESSIBLE]) != 1:
+                continue
+
             for i, intermediate_node in enumerate(self.shortest_paths[src_node][dst_node]):
+                print(intermediate_node)
                 # dist = 0 => divide by 1, dist = 1 => divide by 2 etc.
                 averaged_value = round(desired_value / (i + 1)) # averaged over number of nodes required to obtain
 
@@ -104,16 +114,17 @@ class Optimiser:
         config = Config()
         graphs = []
         for node_id, data in self.base_graph.nodes.items():
-            if data["cls_name"] in NodeType.MULTI_UNCLAIMED:
-                tier, subtier = config.preference_by_id(data["name"], profile_id)
-                if tier > 0 or (tier == 0 and subtier > 0): # desirable and unclaimed
-                    graphs.append(self.dijkstra_multiplier(node_id, tier, subtier))
-                elif tier < 0 or (tier == 0 and subtier < 0): # undesirable and unclaimed
-                    heatmap = copy.deepcopy(self.base_graph)
-                    cost = heatmap.nodes[node_id]["value"] - \
-                           (Optimiser.TIER_VALUE * tier + subtier * Optimiser.SUBTIER_VALUE)
-                    nx.set_node_attributes(heatmap, GraphNode.from_dict(heatmap.nodes[node_id], value=cost).get_dict())
-                    graphs.append(heatmap)
+            if data["cls_name"] not in NodeType.MULTI_UNCLAIMED:
+                continue
+            tier, subtier = config.preference_by_id(data["name"], profile_id)
+            if tier > 0 or (tier == 0 and subtier > 0): # desirable and unclaimed
+                graphs.append(self.dijkstra_multiplier(node_id, tier, subtier))
+            elif tier < 0 or (tier == 0 and subtier < 0): # undesirable and unclaimed
+                heatmap = copy.deepcopy(self.base_graph)
+                cost = heatmap.nodes[node_id]["value"] - \
+                       (Optimiser.TIER_VALUE * tier + subtier * Optimiser.SUBTIER_VALUE)
+                nx.set_node_attributes(heatmap, GraphNode.from_dict(heatmap.nodes[node_id], value=cost).get_dict())
+                graphs.append(heatmap)
 
         if len(graphs) == 0:
             graphs = [self.base_graph]
